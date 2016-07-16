6298535e8ed1d37fe52ceec45029bad7b50f944b
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-07-16 16:49:48 +0900

    add lazy solution

--- lib/my_gen_stage.ex
+++ lib/my_gen_stage.ex
@@ -4,11 +4,23 @@ defmodule MyGenStage do
   def example1() do
     File.read!(@test_file)
     |> String.split("\n")
-    |> Enum.flat_map(fn line ->
+    |> Enum.flat_map(fn line -> # here build a huge list...
       String.split(line, " ")
     end)
     |> Enum.reduce(%{}, fn word, acc ->
       Map.update(acc, word, 1, &(&1 + 1))
+    end)
+    |> Enum.to_list()
+  end
+
+  # lazy solution
+  def example_stream() do
+    File.stream!(@test_file)
+    |> Stream.flat_map(fn line ->
+      String.split(line, " ")
+    end)
+    |> Enum.reduce(%{}, fn word, acc ->
+      Map.update(acc, word, 1, & &1 + 1)
     end)
     |> Enum.to_list()
   end
