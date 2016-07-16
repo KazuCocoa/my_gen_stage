1df547059bbc6d079e4145b88d3040b7522c7eed
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-07-16 16:43:14 +0900

    add example 1

--- lib/my_gen_stage.ex
+++ lib/my_gen_stage.ex
@@ -1,2 +1,15 @@
 defmodule MyGenStage do
+  @test_file "data/sample.txt"
+
+  def example1() do
+    File.read!(@test_file)
+    |> String.split("\n")
+    |> Enum.flat_map(fn line ->
+      String.split(line, " ")
+    end)
+    |> Enum.reduce(%{}, fn word, acc ->
+      Map.update(acc, word, 1, &(&1 + 1))
+    end)
+    |> Enum.to_list()
+  end
 end
