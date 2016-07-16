b2cfb5342fa5e0e771dd178ae90e9accbcaf8fcc
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-07-16 18:56:24 +0900

    add comment and deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule MyGenStage.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:gen_stage]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,8 @@ defmodule MyGenStage.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:gen_stage, "~> 0.1"}
+    ]
   end
 end
